0a62c81da6fb437b436af49cec82a5abfb3def2b
H1rono <hronok66@gmail.com>
2025-01-20 03:52:52 +0900

    :sparkles: Setup tracing, tokio

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    use tracing_subscriber::EnvFilter;
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+
+    Ok(())
 }
