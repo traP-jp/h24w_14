4aef6fc2be2056a4ca01dca286bae3f82daf4bb6
H1rono <hronok66@gmail.com>
2025-01-20 04:51:02 +0900

    :sparkles: Connect to database

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,27 @@
 use anyhow::Context;
+use sqlx::MySqlPool;
 use tokio::net::TcpListener;
 
 use h24w14 as lib;
 
-struct State;
+#[derive(Debug, Clone)]
+struct State {
+    pool: MySqlPool,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    use futures::TryFutureExt;
     use tracing_subscriber::EnvFilter;
 
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
     tracing_subscriber::fmt().with_env_filter(env_filter).init();
 
-    let state = std::sync::Arc::new(State);
+    let pool = load_mysql_from_env("MYSQL_")
+        .or_else(|_| load_mysql_from_env("MARIADB_"))
+        .or_else(|_| load_mysql_from_env("NS_MARIADB_"))
+        .await?;
+    let state = std::sync::Arc::new(State { pool });
 
     let router = lib::router::make(state);
     let tcp_listener = load_tcp_listener().await?;
@@ -21,6 +30,39 @@ async fn main() -> anyhow::Result<()> {
         .await?;
 
     Ok(())
+}
+
+// MARK: helper `fn`s
+
+#[tracing::instrument]
+async fn load_mysql_from_env(prefix: &str) -> anyhow::Result<MySqlPool> {
+    macro_rules! var {
+        ($n:ident) => {{
+            let var_name = format!(concat!("{}", stringify!($n)), prefix);
+            std::env::var(&var_name).with_context(|| format!("Failed to read {var_name}"))
+        }};
+    }
+
+    let hostname = var!(HOSTNAME)?;
+    let user = var!(USER)?;
+    let password = var!(PASSWORD)?;
+    let port: u16 = var!(PORT)?.parse().context("Failed to read PORT value")?;
+    let database = var!(DATABASE)?;
+    let options = sqlx::mysql::MySqlConnectOptions::new()
+        .host(&hostname)
+        .username(&user)
+        .password(&password)
+        .port(port)
+        .database(&database);
+    sqlx::MySqlPool::connect_with(options)
+        .await
+        .inspect_err(|e| {
+            tracing::error!(
+                error = e as &dyn std::error::Error,
+                "Failed to connect database"
+            )
+        })
+        .context("Failed to connect to MySQL")
 }
 
 #[tracing::instrument]
@@ -46,5 +88,13 @@ async fn shutdown() {
             error = &e as &dyn std::error::Error,
             "Failed to watch ctrl-c"
         ),
+    }
+}
+
+// MARK: State `impl`s
+
+impl AsRef<MySqlPool> for State {
+    fn as_ref(&self) -> &MySqlPool {
+        &self.pool
     }
 }
