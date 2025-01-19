00d0907bf932ea1e496f9ab10fdd8453fa7ba6be
H1rono <hronok66@gmail.com>
2025-01-20 04:28:25 +0900

    :sparkles: Setup router

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
+use anyhow::Context;
+use tokio::net::TcpListener;
+
+use h24w14 as lib;
+
+struct State;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     use tracing_subscriber::EnvFilter;
@@ -5,5 +12,39 @@ async fn main() -> anyhow::Result<()> {
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
     tracing_subscriber::fmt().with_env_filter(env_filter).init();
 
+    let state = std::sync::Arc::new(State);
+
+    let router = lib::router::make(state);
+    let tcp_listener = load_tcp_listener().await?;
+    axum::serve(tcp_listener, router)
+        .with_graceful_shutdown(shutdown())
+        .await?;
+
     Ok(())
+}
+
+#[tracing::instrument]
+async fn load_tcp_listener() -> anyhow::Result<TcpListener> {
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or_else(|_| 8000.to_string())
+        .parse()
+        .context("Failed to parse PORT value")?;
+    let addr: std::net::SocketAddr = ([0, 0, 0, 0], port).into();
+    let listener = TcpListener::bind(addr)
+        .await
+        .with_context(|| format!("Failed to bind {addr}"))?;
+    tracing::info!(%addr, "Listening");
+    Ok(listener)
+}
+
+#[tracing::instrument]
+async fn shutdown() {
+    let ctrl_c = tokio::signal::ctrl_c();
+    match ctrl_c.await {
+        Ok(()) => tracing::info!("Received ctrl-c"),
+        Err(e) => tracing::error!(
+            error = &e as &dyn std::error::Error,
+            "Failed to watch ctrl-c"
+        ),
+    }
 }
