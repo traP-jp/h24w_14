8f0090281eee75e0b47aca6677c6bf4b8deef9ed
rei <h.watanabe360@gmail.com>
2025-01-22 13:00:21 +0900

    client: feat: Tailwind CSSのカラーパレットを設定

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "background-default-default": "#FFFFFF",
+        "background-default-secondary": "#F5F5F5",
+        "background-default-tertiary": "#D9D9D9",
+        "text-primary": "#1E1E1E",
+        "text-secondary": "#757575",
+        "text-tertiary": "#B3B3B3",
+        "accent-primary": "#a0d911",
+        "accent-secondary": "#bae637",
+        "accent-tertiary": "#d3f261",
+        "accent-hover": "#7cb305",
+      },
+    },
   },
   plugins: [],
 };
