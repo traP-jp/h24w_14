438d3ef89428a05244d29c7aa33cf5b295095fd9
ras0q <ras0q@users.noreply.github.com>
2025-01-23 22:29:31 +0900

    impl grpc conversion

--- server/src/user/grpc.rs
+++ server/src/user/grpc.rs
@@ -2,6 +2,28 @@ use std::sync::Arc;
 
 use schema::user as schema;
 
+use crate::prelude::IntoStatus;
+
+// MARK: type conversions
+
+impl From<super::User> for schema::User {
+    fn from(value: super::User) -> Self {
+        let super::User {
+            id,
+            name,
+            display_name,
+            created_at,
+            updated_at: _,
+        } = value;
+        Self {
+            id: id.0.to_string(),
+            name,
+            display_name,
+            created_at: Some(created_at.into()),
+        }
+    }
+}
+
 // MARK: ServiceImpl
 
 pub struct ServiceImpl<State> {
@@ -35,8 +57,22 @@ where
 {
     async fn get_user(
         &self,
-        _request: tonic::Request<schema::GetUserRequest>,
+        request: tonic::Request<schema::GetUserRequest>,
     ) -> Result<tonic::Response<schema::GetUserResponse>, tonic::Status> {
-        unimplemented!()
+        let (_, _, schema::GetUserRequest { id }) = request.into_parts();
+        let req = super::GetUser {
+            id: super::UserId(
+                uuid::Uuid::parse_str(&id)
+                    .map_err(|_| tonic::Status::invalid_argument("Invalid UUID"))?,
+            ),
+        };
+        let user = self
+            .state
+            .get_user(req)
+            .await
+            .map_err(IntoStatus::into_status)?
+            .into();
+        let res = schema::GetUserResponse { user: Some(user) };
+        Ok(tonic::Response::new(res))
     }
 }
