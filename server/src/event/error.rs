c2d7ecc9f3934c4e986df6724e61bb5361c2fa77
H1rono <hronok66@gmail.com>
2025-01-24 06:55:55 +0900

    :sparkles: Impl `EventService`

---
+++ server/src/event/error.rs
@@ -0,0 +1,23 @@
+use tokio::sync::broadcast::error::{RecvError, SendError};
+use tokio_stream::wrappers::errors::BroadcastStreamRecvError;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    SendMessage(#[from] SendError<super::Message>),
+    #[error(transparent)]
+    SendEvent(#[from] SendError<super::Event>),
+    #[error(transparent)]
+    Recv(#[from] RecvError),
+    #[error(transparent)]
+    RecvStream(#[from] BroadcastStreamRecvError),
+}
+
+impl From<Error> for tonic::Status {
+    fn from(value: Error) -> Self {
+        tracing::error!(error = &value as &dyn std::error::Error, "channel error");
+        tonic::Status::internal("")
+    }
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
---
+++ server/src/event/impl.rs
@@ -0,0 +1,64 @@
+use futures::stream::{BoxStream, StreamExt, TryStreamExt};
+use tokio::sync::broadcast;
+use tokio_stream::wrappers::BroadcastStream;
+
+impl super::EventChannels {
+    pub fn new(capacity: usize) -> Self {
+        let (message_tx, _) = broadcast::channel(capacity);
+        let (event_tx, _) = broadcast::channel(capacity);
+        Self {
+            message_tx,
+            event_tx,
+        }
+    }
+}
+
+impl<Context> super::EventService<Context> for super::EventServiceImpl
+where
+    Context: AsRef<super::EventChannels>,
+{
+    type Error = super::Error;
+
+    fn subscribe_messages<'a>(
+        &'a self,
+        ctx: &'a Context,
+    ) -> BoxStream<'a, Result<crate::message::Message, Self::Error>> {
+        let channels = ctx.as_ref();
+        let rx = channels.message_tx.subscribe();
+        BroadcastStream::new(rx).map_err(super::Error::from).boxed()
+    }
+
+    fn subscribe_events<'a>(
+        &'a self,
+        ctx: &'a Context,
+    ) -> BoxStream<'a, Result<super::Event, Self::Error>> {
+        let channels = ctx.as_ref();
+        let rx = channels.event_tx.subscribe();
+        BroadcastStream::new(rx).map_err(super::Error::from).boxed()
+    }
+
+    fn publish_event<'a>(
+        &'a self,
+        ctx: &'a Context,
+        event: super::Event,
+    ) -> futures::future::BoxFuture<'a, Result<(), Self::Error>> {
+        use futures::FutureExt;
+
+        let channels = ctx.as_ref();
+        publish_event(channels, event).boxed()
+    }
+}
+
+#[tracing::instrument(skip_all)]
+async fn publish_event(
+    channels: &super::EventChannels,
+    event: super::Event,
+) -> super::error::Result<()> {
+    if let super::Event::Message(message) = &event {
+        let subscribers = channels.message_tx.send(message.clone())?;
+        tracing::trace!(subscribers, "Published message");
+    }
+    let subscribers = channels.event_tx.send(event)?;
+    tracing::trace!(subscribers, "Published event");
+    Ok(())
+}
