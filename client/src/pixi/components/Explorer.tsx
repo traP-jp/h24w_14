614d2095543a93d150948110eedd1cd0e453a62f
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-01-24 23:04:31 +0900

    :recycle: drawにuseCallback使う

--- client/src/pixi/components/Explorer.tsx
+++ client/src/pixi/components/Explorer.tsx
@@ -1,7 +1,8 @@
 import { Sprite, Text, Graphics } from "@pixi/react";
 import { DisplayPosition, Position } from "../Position";
-import { TextStyle } from "pixi.js";
+import { Graphics as PIXIGraphics, TextStyle } from "pixi.js";
 import { themeColors } from "../theme";
+import { useCallback } from "react";
 
 export type Props =
   | {
@@ -22,24 +23,25 @@ const iconSize = 50;
 const Explorer: React.FC<Props> = ({ position, imgURL, isMe, name }) => {
   const pos = isMe ? { x: position.left, y: position.top } : position;
 
+  const draw = useCallback(
+    (g: PIXIGraphics) => {
+      g.clear();
+      g.lineStyle(1, themeColors.textSecondary);
+      g.beginFill(themeColors.backgroundSecondary);
+      g.drawRoundedRect(
+        pos.x - iconSize / 2,
+        pos.y - iconSize / 2,
+        iconSize,
+        iconSize,
+        0,
+      );
+    },
+    [pos.x, pos.y],
+  );
+
   return (
     <>
-      {isMe && (
-        <Graphics
-          draw={(g) => {
-            g.clear();
-            g.lineStyle(1, themeColors.textSecondary);
-            g.beginFill(themeColors.backgroundSecondary);
-            g.drawRoundedRect(
-              pos.x - iconSize / 2,
-              pos.y - iconSize / 2,
-              iconSize,
-              iconSize,
-              0,
-            );
-          }}
-        />
-      )}
+      {isMe && <Graphics draw={draw} />}
       <Sprite
         image={imgURL}
         {...pos}
