3c86a943ed4e5d951fb4d4a5cf30ab031e3c4fc9
H1rono <hronok66@gmail.com>
2025-01-24 23:50:02 +0900

    :sparkles: Add `traq::user::Error` variants

--- server/src/traq/user/error.rs
+++ server/src/traq/user/error.rs
@@ -2,8 +2,12 @@
 pub enum Error {
     #[error("Not found")]
     NotFound,
+    #[error("Received unexpected response from traQ")]
+    UnexpectedResponseFromTraq,
     #[error(transparent)]
     Sqlx(#[from] sqlx::Error),
+    #[error(transparent)]
+    Reqwest(#[from] reqwest::Error),
     #[error(transparent)]
     Status(#[from] tonic::Status),
 }
@@ -12,9 +16,17 @@ impl From<Error> for tonic::Status {
     fn from(value: Error) -> Self {
         match value {
             Error::NotFound => tonic::Status::not_found("Not found"),
+            Error::UnexpectedResponseFromTraq => {
+                tracing::warn!("Received unexpected response from traQ");
+                tonic::Status::unknown("Received unexpected response from traQ")
+            }
             Error::Sqlx(e) => {
                 tracing::error!(error = &e as &dyn std::error::Error, "Database error");
                 tonic::Status::internal("database error")
+            }
+            Error::Reqwest(e) => {
+                tracing::error!(error = &e as &dyn std::error::Error, "HTTP request error");
+                tonic::Status::unknown("HTTP request error")
             }
             Error::Status(s) => {
                 tracing::error!(error = &s as &dyn std::error::Error, "Unexpected");
