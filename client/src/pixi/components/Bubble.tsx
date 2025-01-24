940d42172a2f506cc73df1f7eae31e5b52624149
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-01-24 22:25:48 +0900

    :lipstick: リアクションのスタイル変更

--- client/src/pixi/components/Bubble.tsx
+++ client/src/pixi/components/Bubble.tsx
@@ -1,42 +1,67 @@
-import { Graphics } from "@pixi/react";
-import React from "react";
+import { Graphics, Container } from "@pixi/react";
+import React, { useEffect, useRef, useState } from "react";
 import PIXI, { ColorSource } from "pixi.js";
 
 interface Props {
   x: number;
   y: number;
-  width: number;
-  height: number;
+  padding: number;
   lineWidth: number;
   radius: number;
   direction?: "up";
   color: ColorSource;
   fillColor?: ColorSource;
   fillAlpha?: number;
+  children?: React.ReactNode;
 }
 
+const bubbleTriangleHeight = 5;
+const bubbleTriangleWidth = 10;
+
 const Bubble: React.FC<Props> = (props) => {
+  const containerRef = useRef<PIXI.Container>(null);
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [containerHeight, setContainerHeight] = useState(0);
+
   const draw = (g: PIXI.Graphics) => {
     g.clear();
     g.lineStyle(props.lineWidth, props.color);
     if (props.fillColor !== undefined) {
       g.beginFill(props.fillColor, props.fillAlpha ?? 1);
     }
-    g.drawRoundedRect(0, 0, props.width, props.height, props.radius);
-    g.moveTo(props.width / 2, props.height + 5);
-    g.lineTo(props.width / 2 - 5, props.height);
-    g.lineTo(props.width / 2 + 5, props.height);
-    g.lineTo(props.width / 2, props.height + 5);
+    g.drawRoundedRect(0, 0, containerWidth, containerHeight, props.radius);
+    g.moveTo(containerWidth / 2, containerHeight + bubbleTriangleHeight);
+    g.lineTo(containerWidth / 2 - bubbleTriangleWidth / 2, containerHeight);
+    g.lineTo(containerWidth / 2 + bubbleTriangleWidth / 2, containerHeight);
+    g.lineTo(containerWidth / 2, containerHeight + bubbleTriangleHeight);
     if (props.fillColor !== undefined) {
       g.endFill();
     }
   };
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const containerRec = containerRef.current.getBounds();
+      setContainerWidth(containerRec.width + props.padding * 2);
+      setContainerHeight(containerRec.height + props.padding * 2);
+    }
+  }, [containerRef, props.padding]);
+
   return (
-    <Graphics
-      draw={draw}
-      x={-props.width / 2 + props.x}
-      y={-props.height + props.y}
-    />
+    <>
+      <Graphics
+        draw={draw}
+        x={-containerWidth / 2 + props.x}
+        y={-containerHeight + props.y - props.padding}
+      />
+      <Container
+        x={-containerWidth / 2 + props.padding}
+        y={-containerHeight / 2 - containerHeight}
+        ref={containerRef}
+      >
+        {props.children}
+      </Container>
+    </>
   );
 };
 
--- client/src/pixi/components/Reaction.tsx
+++ client/src/pixi/components/Reaction.tsx
@@ -1,10 +1,9 @@
 import { Container, Sprite, Text } from "@pixi/react";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { ReactionAssets, ReactionName } from "../reactions";
 import { Position } from "../Position";
-import { themeColors } from "../theme";
 import Bubble from "./Bubble";
-import PIXI, { TextStyle } from "pixi.js";
+import { TextStyle } from "pixi.js";
 
 interface Props {
   position: Position;
@@ -20,17 +19,6 @@ const userIconSize = 14;
 
 const Reaction: React.FC<Props> = ({ position, reaction, user }) => {
   const [showUser, setShowUser] = useState(false);
-  const iconAndNameRef = useRef<PIXI.Container>(null);
-  const [iconAndNameWidth, setIconAndNameWidth] = useState(0);
-
-  useEffect(() => {
-    if (!showUser) {
-      return;
-    }
-    if (iconAndNameRef.current) {
-      setIconAndNameWidth(iconAndNameRef.current.width);
-    }
-  }, [showUser, iconAndNameRef]);
 
   return (
     <Container
@@ -51,39 +39,31 @@ const Reaction: React.FC<Props> = ({ position, reaction, user }) => {
         <>
           <Bubble
             x={0}
-            y={-reactionImageSize / 2 - 5}
+            y={-reactionImageSize / 2}
+            padding={2.5}
             radius={10}
-            color={themeColors.accentSecondary}
-            height={20}
-            width={iconAndNameWidth + 10}
+            color={"#000000"}
             fillColor={"#000000"}
             lineWidth={2}
-          />
-          <Container
-            x={-iconAndNameWidth / 2}
-            y={-reactionImageSize / 2 - 15}
-            ref={iconAndNameRef}
           >
             <Sprite
               image={user.iconURL}
               width={userIconSize}
               height={userIconSize}
-              anchor={{ x: 0, y: 0.5 }}
             />
             <Text
-              anchor={{ x: 0, y: 0.5 }}
               text={user.name}
               x={userIconSize + 5}
               y={0}
               style={
                 new TextStyle({
-                  fill: themeColors.textSecondary,
+                  fill: "#ffffff",
                   fontSize: 14,
                   fontWeight: "bold",
                 })
               }
             />
-          </Container>
+          </Bubble>
         </>
       )}
     </Container>
