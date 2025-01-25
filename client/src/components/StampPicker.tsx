329933f6c47edb1bbc57c681c140e1803a39f922
rei <h.watanabe360@gmail.com>
2025-01-26 00:41:26 +0900

    feat: StampPickerコンポーネントを修正

--- client/src/components/StampPicker.tsx
+++ client/src/components/StampPicker.tsx
@@ -1,20 +1,50 @@
 import { SmileOutlined } from "@ant-design/icons";
 import { Button, Popover } from "antd";
 import React, { useState } from "react";
+import Reaction, { ReactionAssets } from "../model/reactions";
 
 export const StampPicker: React.FC = () => {
   const [isPopoverOpen, setPopoverOpen] = useState(false);
-  const icons = Array(20).fill(<SmileOutlined />);
+  const reactions: Reaction[] = Array(8)
+    .fill([
+      {
+        id: "1",
+        userId: "1",
+        position: { x: 0, y: 0 },
+        kind: "iine",
+        createdAt: new Date(),
+        expiresAt: new Date(),
+      },
+      {
+        id: "2",
+        userId: "2",
+        position: { x: 0, y: 0 },
+        kind: "kusa",
+        createdAt: new Date(),
+        expiresAt: new Date(),
+      },
+      {
+        id: "3",
+        userId: "3",
+        position: { x: 0, y: 0 },
+        kind: "pro",
+        createdAt: new Date(),
+        expiresAt: new Date(),
+      },
+    ])
+    .flat();
 
   return (
     <Popover
       content={
         <div className="grid grid-cols-4 gap-2">
-          {icons.map((icon, index) => (
+          {reactions.map((reaction, index) => (
             <Button
               key={index}
               type="text"
-              icon={icon}
+              icon={
+                <img src={ReactionAssets[reaction.kind]} alt={reaction.kind} />
+              }
               className="hover:bg-background-secondary"
             />
           ))}
