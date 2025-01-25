ef07734dc31a852fe37e26968d8f86d3fec62a24
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-01-25 11:23:05 +0900

    :adhesive_bandage: useMe

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 import { UserServiceClient } from "../schema/UserServiceClientPb";
 import serverHostName from "./hostname";
-import { GetUserRequest } from "../schema/user_pb";
+import { GetMeRequest, GetUserRequest } from "../schema/user_pb";
 
 const userClient = new UserServiceClient(serverHostName);
 
@@ -10,4 +10,10 @@ export const useUser = (userId: string) => {
   req.setId(userId);
   const fetcher = () => userClient.getUser(req);
   return useSWR(`user/${userId}`, fetcher);
+};
+
+export const useMe = () => {
+  const req = new GetMeRequest();
+  const fetcher = () => userClient.getMe(req);
+  return useSWR(`me`, fetcher);
 };
