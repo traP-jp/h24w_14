f6ac9eb9d15180354e14090b6f2427f8c9e6b2e5
rei <h.watanabe360@gmail.com>
2025-01-25 16:50:19 +0900

    fix: Appコンポーネントからコードを削除

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,7 @@
 import Canvas from "./pixi/Canvas";
 import { Timeline } from "./timeline/Timeline";
-import useExplorerDispatcher from "./api/explorer";
-import { useSetAtom } from "jotai";
-import dispatcherAtom from "./state/dispatcher";
 
 const App = () => {
-  const dispatcher = useExplorerDispatcher();
-  useSetAtom(dispatcherAtom)(dispatcher);
-
   return (
     <div className="flex">
       <Canvas />
