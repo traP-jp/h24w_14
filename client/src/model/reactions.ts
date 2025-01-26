fa282719cc414d0ee789c4602f417eaea49e1b37
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-01-26 14:40:38 +0900

    リアクション追加

--- client/src/model/reactions.ts
+++ client/src/model/reactions.ts
@@ -1,14 +1,19 @@
 import Iine from "../assets/reactions/iine.webp";
 import Kusa from "../assets/reactions/kusa.webp";
 import Pro from "../assets/reactions/pro.webp";
+import Fire from "../assets/reactions/fire.png";
+import Smile from "../assets/reactions/smile.png";
+
 import { Position } from "./position";
 
-export type ReactionName = "iine" | "kusa" | "pro";
+export type ReactionName = "iine" | "kusa" | "pro" | "fire" | "smile";
 
 export const ReactionAssets = {
   iine: Iine,
   kusa: Kusa,
   pro: Pro,
+  fire: Fire,
+  smile: Smile,
 };
 
 interface Reaction {
