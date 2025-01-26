e1c9db4e9c77c438260354b57f94f085d2a45f2d
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2025-01-26 18:30:42 +0900

    ♻️ useCallback つけてるところにつける

--- client/src/pixi/hooks/message.ts
+++ client/src/pixi/hooks/message.ts
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Message } from "../../model/message";
 
 const useMessageExpanded = () => {
   const [expanded, setExpanded] = useState(false);
   const [message, setMessage] = useState<Message | null>(null);
 
-  const expandMessage = (message: Message) => {
+  const expandMessage = useCallback((message: Message) => {
     setExpanded(true);
     setMessage(message);
-  };
+  }, []);
 
-  const collapseMessage = () => {
+  const collapseMessage = useCallback(() => {
     setExpanded(false);
     setMessage(null);
-  };
+  }, []);
 
   return { expanded, message, expandMessage, collapseMessage };
 };
