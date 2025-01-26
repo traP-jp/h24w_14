da2dcd1f5e646b6af09692b1890bc07e7f0609d1
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-01-26 13:41:42 +0900

    :wrench: serverのconfig

--- client/src/api/hostname.ts
+++ client/src/api/hostname.ts
@@ -1,3 +1,11 @@
-const serverHostName = "http://localhost:8000";
-export const serverWSHostName = "ws://localhost:8000/ws";
+const host = URL.parse(import.meta.env.BASE_URL)?.host;
+
+const serverHostName = import.meta.env.DEV
+  ? "http://localhost:8000"
+  : `https://${host}`;
+
+export const serverWSHostName = import.meta.env.DEV
+  ? "ws://localhost:8000/ws"
+  : `ws://${host}/ws`;
+
 export default serverHostName;
